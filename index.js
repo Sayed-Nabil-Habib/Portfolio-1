6e5009cafd8b5d975cd29086f7fddf824a044dc6
Sayed-Nabil-Habib <nabilhabib2015@yahoo.com>
2023-05-30 18:35:44 +0200

    js updated

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const checkbox = document.getElementById('toggleCheckbox');
 const body = document.body;
+const sectionFirst = document.querySelector(".section-first");
+const sectionSecond = document.querySelector(".section-second");
+const getInTouch = document.querySelector(".get-inTouch");
+
+const sectionFirstText = sectionFirst.querySelectorAll("p, h2");
+const sectionSecondText = sectionSecond.querySelectorAll("h1, h2");
+const getInTouchText = getInTouch.querySelectorAll("h1, p");
 
 checkbox.addEventListener('change', function() {
   if (checkbox.checked) {
     body.style.backgroundColor = 'white';
-    body.style.color = "#0a192f";
-  }  else {
+    body.style.transition = 'color 0.3s ease';
+    sectionFirstText.forEach(element => {
+      element.style.color = '#0a192f';
+      element.style.transition = 'color 0.3s ease';
+    });
+    sectionSecondText.forEach(element => {
+      element.style.color = '#0a192f';
+      element.style.transition = 'color 0.3s ease';
+    });
+    getInTouchText.forEach(element => {
+      element.style.color = '#0a192f';
+      element.style.transition = 'color 0.3s ease';
+    });
+
+  } else {
     body.style.backgroundColor = '';
+    sectionFirstText.forEach(element => {
+      element.style.color = '';
+      element.style.transition = 'color 0.3s ease';
+    });
+    sectionSecondText.forEach(element => {
+      element.style.color = '';
+      element.style.transition = 'color 0.3s ease';
+    });
+    getInTouchText.forEach(element => {
+      element.style.color = '';
+      element.style.transition = 'color 0.3s ease';
+    });
   }
 });
