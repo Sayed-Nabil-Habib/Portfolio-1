3f4984134216eaf5fafa0114651759155fb1a2e2
Sayed-Nabil-Habib <nabilhabib2015@yahoo.com>
2023-05-30 17:28:44 +0200

    js code added for the dark and light mode

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const body = document.body;
 
 checkbox.addEventListener('change', function() {
   if (checkbox.checked) {
+    body.style.backgroundColor = 'white';
+    body.style.color = "#0a192f";
+  }  else {
     body.style.backgroundColor = '';
-  } else {
-    body.style.backgroundColor = 'yellow';
   }
 });
