90a39b8c3f82bfaabd06c021c0a361a463c83dbb
Sayed-Nabil-Habib <nabilhabib2015@yahoo.com>
2023-06-07 20:15:57 +0200

    animate fade-in added to cards

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+const cardContainers = document.querySelectorAll(".project-card");
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+  );
+}
+
+function handleScroll() {
+  cardContainers.forEach((container) => {
+    if (isInViewport(container)) {
+      container.classList.add("animate-fade-in");
+    }
+  });
+}
+
+window.addEventListener("scroll", handleScroll);
